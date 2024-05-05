f85728e24f25d5ddf5d752bf3e8996405ed5c31b
Stephen F. Booth <me@sbooth.org>
2024-05-04 20:36:53 -0400

    Swift/C++ interoperability
    Reqires Swift 5.9

--- Tests/CXXAudioUtilitiesTests/CXXAudioUtilitiesTests.swift
+++ Tests/CXXAudioUtilitiesTests/CXXAudioUtilitiesTests.swift
@@ -1,4 +1,31 @@
 import XCTest
+#if swift(>=5.9)
+@testable import CXXAudioUtilities
+#endif
 
 final class CXXAudioUtilitiesTests: XCTestCase {
+#if swift(>=5.9)
+	func testByteStream() {
+		let bs = SFB.ByteStream()
+		XCTAssert(bs.Length() == 0)
+		XCTAssert(bs.Remaining() == 0)
+		XCTAssert(bs.Position() == 0)
+	}
+
+	func testStreamDescription() {
+		let asbd = SFB.CAStreamBasicDescription(.float32, 44100, 2, false)
+		XCTAssert(asbd.IsPCM() == true)
+		XCTAssert(asbd.IsFloat() == true)
+		XCTAssert(asbd.IsInteger() == false)
+		XCTAssert(asbd.mSampleRate == 44100)
+		XCTAssert(asbd.ChannelCount() == 2)
+		XCTAssert(asbd.IsInterleaved() == false)
+		XCTAssert(asbd.IsNonInterleaved() == true)
+	}
+
+	func testChannelLayout() {
+		let stereo = SFB.CAChannelLayout.Stereo
+		XCTAssert(stereo.ChannelCount() == 2)
+	}
+#endif
 }
