27964ba45ff672594a23345c51e12a55aefc55d9
Stephen F. Booth <me@sbooth.org>
2024-12-20 11:37:47 -0600

    Use `%g` instead of `std::rint`

--- Sources/CXXAudioUtilities/SFBCAStreamBasicDescription.cpp
+++ Sources/CXXAudioUtilities/SFBCAStreamBasicDescription.cpp
@@ -124,10 +124,7 @@ CFStringRef SFB::CAStreamBasicDescription::CopyFormatDescription() const noexcep
 	CFMutableStringRef result = CFStringCreateMutable(kCFAllocatorDefault, 0);
 
 	// Channels and sample rate
-	if(std::rint(mSampleRate) == mSampleRate)
-		CFStringAppendFormat(result, nullptr, CFSTR("%u ch @ %lld Hz, "), mChannelsPerFrame, static_cast<uint64_t>(mSampleRate));
-	else
-		CFStringAppendFormat(result, nullptr, CFSTR("%u ch @ %.2f Hz, "), mChannelsPerFrame, mSampleRate);
+	CFStringAppendFormat(result, nullptr, CFSTR("%u ch @ %g Hz, "), mChannelsPerFrame, mSampleRate);
 
 	// Shorter description for common formats
 	if(auto commonFormat = CommonFormat(); commonFormat.has_value()) {
